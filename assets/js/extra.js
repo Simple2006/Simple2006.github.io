09cc0e145f0da497ce2e67019e26c535de2ddd7b
Simple <vinayven01@gmail.com>
2021-03-14 00:45:02 -0800

    added imgs + more js

---
+++ assets/js/extra.js
@@ -0,0 +1,7 @@
+function gitredirect() {
+    window.open("https://github.com/Simple2006/libmaths", "_blank")
+}
+
+function pypiredirect() {
+    window.open("https://pypi.org/project/libmaths/", "_blank")
+}
